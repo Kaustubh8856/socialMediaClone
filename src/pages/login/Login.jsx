42aecf6190dc878f913221f54558469836cb4f3f
Kaustubh <kaustubh8856@gmail.com>
2024-04-17 14:51:44 +0530

    LogInPage

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,34 @@
-import "./login.scss"
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            quam nihil dicta facere soluta quaerat error veritatis molestiae
+            recusandae, explicabo velit magni corrupti hic ex qui, vel
+            temporibus numquam non cum quae ullam voluptatum perspiciatis
+            expedita. Impedit, perferendis dicta! Quo nostrum dignissimos vitae?
+            Qui unde nesciunt odit id dolores. Beatae!
+          </p>
+          <span>Don't have an account?</span>
+          <button>Register</button>
+        </div>
 
-export default Login
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
