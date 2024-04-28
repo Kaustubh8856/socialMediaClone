2f829056db32862759dcad986194941d50c2e953
Kaustubh <kaustubh8856@gmail.com>
2024-04-28 12:09:25 +0530

    rightBar completed

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -33,6 +33,113 @@ const RightBar = () => {
             </div>
           </div>
         </div>
+
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="img"
+              />
+              <p>
+                <span>KSW</span> changed their cover picture.
+              </p>
+            </div>
+            <span>1min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="img"
+              />
+              <p>
+                <span>KSW</span> liked your photo.
+              </p>
+            </div>
+            <span>5min ago</span>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="img"
+              />
+              <p>
+                <span>KSW</span> commented on your post.
+              </p>
+            </div>
+            <span> 35min ago</span>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="img"
+              />
+              <p>
+                <span>KSW</span> replied to you.
+              </p>
+            </div>
+            <span>1hr ago</span>
+          </div>
+        </div>
+
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="img"
+              />
+              <div className="online"></div>
+              <p>
+                <span>KSW</span>
+              </p>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="img"
+              />
+              <div className="online"></div>
+              <p>
+                <span>KSW</span>
+              </p>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="img"
+              />
+              <div className="online"></div>
+              <p>
+                <span>KSW</span>
+              </p>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="img"
+              />
+              <div className="online"></div>
+              <p>
+                <span>KSW</span>
+              </p>
+            </div>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
